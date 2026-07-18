7e4d7284f6ad872ddc5b731aafb8d32a3c2f1648
fujitani sora <fujitanisora0414@gmail.com>
2026-07-18 22:48:57 +0900

    test: extend parity harness to metadata and comments

---
+++ ext/rfmt/tests/fixtures/parity/metadata_classes.rb
@@ -0,0 +1,24 @@
+class Foo::Bar < Baz::Qux
+  def change
+    x = compute(1)
+    @count = x
+  end
+end
+
+class AddUsers < ActiveRecord::Migration[8.1]
+  def change
+    create_table :users
+  end
+end
+
+class Simple < Base
+end
+
+class Plain
+end
+
+module Alpha::Beta
+end
+
+module Gamma
+end
---
+++ ext/rfmt/tests/fixtures/parity/metadata_conditionals.rb
@@ -0,0 +1,14 @@
+result = flag ? 1 : 2
+
+if flag
+  a = 1
+else
+  a = 2
+end
+
+unless flag
+  b = 3
+end
+
+c = 4 if flag
+d = 5 unless flag
